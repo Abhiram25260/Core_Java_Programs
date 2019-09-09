ac9d16f8f43956d8c92f5fd1ecc8c0ce70cb6d78
Abhiram T <abhiram.att@gmail.com>
2019-09-09 20:24:37 +0530

    Updated LinkedList Program

--- Core Java Programming/src/LinkedList/ImplimentationOfLinkList.java
+++ Core Java Programming/src/LinkedList/ImplimentationOfLinkList.java
@@ -5,6 +5,7 @@ public class ImplimentationOfLinkList {
 	static LinkedList<Integer> llIntegers=new LinkedList<>();
 	public static void main(String ...arg) {
 		scanner=new Scanner(System.in);
+		while(true) {
 		System.out.println("PRESS 1 TO INSERT_FRONT");
 		System.out.println("PRESS 2 TO INSERT_REAR");
 		System.out.println("PRESS 3 TO INSERT_AT_INDEX");
@@ -18,13 +19,13 @@ public class ImplimentationOfLinkList {
 		
 		switch (choice) {
 		case 1:
-			//insert_front();
+			insert_Front();
 			break;
 		case 2:
-			
+			insert_Rear();
 			break;
 		case 3:
-			
+			insert_At_Index();
 			break;
 		case 4:
 			
@@ -46,6 +47,31 @@ public class ImplimentationOfLinkList {
 			System.exit(0);
 			break;
 		}
+		}
+	}
+	//INPUT FUNCTION THAT WILL RETURN INPUT FOR NEW NODE
+	static int input() {
+		System.out.println("Enter the Element :");
+		int item=scanner.nextInt();
+		return item;
+	}
+	//INDEX FUNCTION THAT WILL RETURN INDEX IN WHICH OPERATION IS TO BE DONE
+	static int index() {
+		System.out.println("ENter the Index");
+		int index=scanner.nextInt();
+		return index;
+	}
+	//FOR INSERTING AT FRONT
+	static void insert_Front() {
+		llIntegers.addFirst(input());
+	}
+	//FOR INSERTING AT THE END
+	static void insert_Rear() {
+		llIntegers.addLast(input());
+	}
+	//FOR INSERTING AT SPECIFIED INDEX
+	static void insert_At_Index() {
+		llIntegers.add(index(),input());
 	}
 
 }
